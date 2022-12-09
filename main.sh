058c182ded0e02492560832946d7741ecbe136a7
CyberJaindu <120081413+CyberJaindu@users.noreply.github.com>
2022-12-09 11:09:36 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -11,28 +11,32 @@ lb='\e[1;36m'
 
 echo
 
-echo -e $g "█████╗    ██████╗"
-echo -e $g "██╔══██╗██╔════╝"
-echo -e $g "██║    ╚═╝██║     ██╗"
-echo -e $g "██║    ██╗██║    ╚██╗"
-echo -e $g "╚█████╔╝╚██████╔╝"
-echo -e $g "  ╚════╝    ╚═════╝"
 
-echo -e $g "██████╗    █████╗  ███╗    ██╗███╗    ██╗███████╗██████╗"
-echo -e $g "██╔══██╗██╔══██╗████╗  ██║████╗  ██║██╔════╝██╔═██╗"
-echo -e $g "██████╦╝███████║██╔██╗██║██╔██╗██║█████╗    ██████╔╝"
-echo -e $g "██╔══██╗██╔══██║██║╚████║██║╚████║██╔══╝    ██╔══██╗"
-echo -e $g "██████╦╝██║    ██║██   ╚███║ ██║  ╚███║███████╗██║     ██║"
-echo -e $g "╚═════╝ ╚═╝   ╚═╝╚═╝   ╚══╝╚═╝    ╚══╝╚══════╝╚═╝     ╚═╝"
-echo -e $r "                                                TOOL BY CYBER JAINDU"
+echo -e $g "     ██╗      ██████╗  █████╗ ███   ██╗███████╗██████╗"
+echo -e $g "     ██║      ██╔══██╗██╔══██╗████╗ ██║██╔════╝██╔══██╗"
+echo -e $g "     ██║█████╗██████╦╝███████║██╔██╗██║█████╗  ██████╔╝"
+echo -e $g "██╗  ██║╚════╝██╔══██╗██╔══██║██║╚████║██╔══╝  ██╔══██╗"
+echo -e $g "╚█████╔╝      ██████╦╝██║  ██║██║ ╚███║███████╗██║  ██║"
+echo -e $g " ╚════╝       ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚══╝╚══════╝╚═╝  ╚═╝"
+echo -e $r "                                   TOOL BY CYBER JAINDU"
+echo
+echo
+echo
+echo
+
+
+
+
+
+
 echo -e $b "WHAT IS YOUR BANNER NAME ? : \c"
-read Bname
+read varBname
 echo 
 echo -e $b "WHAT IS YOUR NAME ? : \c"
-read name
+read varname
 
-echo "cowsay -f eyes "$name" | lolcat " > name.txt
-echo "figlet "$Bname" | lolcat " > Bname.txt
+echo "cowsay -f eyes "$varname" | lolcat " > name.txt
+echo "figlet "$varBname" | lolcat " > Bname.txt
 echo "clear" > clear.txt
 echo "PS1='\$ ' " > Temp.txt
 
@@ -53,7 +57,28 @@ cat "name.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
 cat "Bname.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
 cat "Temp.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
 
-rm -rf name.txt Bname.txt clear.txt Temp.txt
+rm -rf name.txt Baname.txt clear.txt Temp.txt
 echo
 figlet FINISH | lolcat
-echo "PLEASE RESTART YOUR TERMUX"
+echo "PLEASE RESTART YOUR TERMUX"\c"
+read varname
+echo "cowsay -f eyes "$varname" | lolcat " > name.txt
+echo "figlet "$varBname" | lolcat " > Bname.txt
+echo "clear" > clear.txt
+echo "PS1='\$ ' " > Temp.txt
+#remove old files
+rm -rf /data/data/com.termux/files/usr/etc/zshrc
+rm -rf /data/data/com.termux/files/usr/etc/bash.bashrc
+#inject files to zshrc
+cat "clear.txt" >> /data/data/com.termux/files/usr/etc/zshrc
+cat "name.txt" >> /data/data/com.termux/files/usr/etc/zshrc
+cat "Bname.txt" >> /data/data/com.termux/files/usr/etc/zshrc
+cat "Temp.txt" >> /data/data/com.termux/files/usr/etc/zshrc
+#inject files to bash.bashrc
+cat "clear.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+cat "name.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+cat "Bname.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+cat "Temp.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+rm -rf name.txt Baname.txt clear.txt Temp.txt
+echo
+figlet FINISH | lolcat
