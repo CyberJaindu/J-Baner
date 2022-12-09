316c00a1a1572bfa14d6752101ffe66b6a889aab
Cyber-Jaindu <120081413+CyberJaindu@users.noreply.github.com>
2022-12-09 16:30:19 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
-#color
+/#color
 r='\e[1;31m'
 g='\e[1;32m'
 y='\e[1;33m'
@@ -31,7 +31,7 @@ echo
 
 echo -e $b "WHAT IS YOUR BANNER NAME ? : \c"
 read varBname
-echo 
+echo
 echo -e $b "WHAT IS YOUR NAME ? : \c"
 read varname
 
@@ -60,25 +60,4 @@ cat "Temp.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
 rm -rf name.txt Baname.txt clear.txt Temp.txt
 echo
 figlet FINISH | lolcat
-echo "PLEASE RESTART YOUR TERMUX"\c"
-read varname
-echo "cowsay -f eyes "$varname" | lolcat " > name.txt
-echo "figlet "$varBname" | lolcat " > Bname.txt
-echo "clear" > clear.txt
-echo "PS1='\$ ' " > Temp.txt
-#remove old files
-rm -rf /data/data/com.termux/files/usr/etc/zshrc
-rm -rf /data/data/com.termux/files/usr/etc/bash.bashrc
-#inject files to zshrc
-cat "clear.txt" >> /data/data/com.termux/files/usr/etc/zshrc
-cat "name.txt" >> /data/data/com.termux/files/usr/etc/zshrc
-cat "Bname.txt" >> /data/data/com.termux/files/usr/etc/zshrc
-cat "Temp.txt" >> /data/data/com.termux/files/usr/etc/zshrc
-#inject files to bash.bashrc
-cat "clear.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
-cat "name.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
-cat "Bname.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
-cat "Temp.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
-rm -rf name.txt Baname.txt clear.txt Temp.txt
-echo
-figlet FINISH | lolcat
+echo -e $r "PLEASE RESTART YOUR TERMUX"
